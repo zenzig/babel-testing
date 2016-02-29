fd703b135f1d03b36cbb234e180f4ee067478de2
Rich Olson <rich@zenzig.com>
2016-02-29 13:20:14 -0600

    add links to ui-sref

--- public/client/src/app.js
+++ public/client/src/app.js
@@ -2,32 +2,56 @@ import angular from 'angular';
 import 'angular-ui-router';
 
 angular.module('olympics',["ui.router"  ])
+
+
 .config(($stateProvider, $urlRouterProvider) => {
-  
-    $urlRouterProvider.otherwise("/sports");
+    $urlRouterProvider.otherwise('/sports');
     
     $stateProvider
-    
     .state('sports', {
-        url: "/sports",
-        templateUrl: "client/sports/sports-nav.html"
+        url: '/sports',
+        templateUrl: 'client/sports/sports-nav.html',
+        resolve: {
+          sportsService: function($http) {
+            return $http.get('http://admin.zenzig.com:3000/api/sports');
+          }  
+        },
+        
+        controller: function(sportsService){
+          this.sports = sportsService.data; 
+        },
+        controllerAs: 'sportsCtrl'
     })
     
     .state('sports.medals', {
         url: "/:sportName",
-        templateUrl: "client/sports/sports-medals.html"
+        templateUrl: "client/sports/sports-medals.html",
+        resolve: {
+            sportService: function($q){
+                return $q((resolve, reject) => {
+                    let sport = {
+                        "name": "Cycling",
+                        "goldMedals": [{
+                            "division": "Womens",
+                            "country": "Australia",
+                            "year": 2012
+                        }, {
+                            "division": "Mens",
+                            "country": "UK",
+                            "year": 2012
+                        }]
+                    };
+                    resolve({data: sport});
+                })
+                
+            }
+        },
+        controller: function(sportService){
+            this.sport = sportService.data;
+        },
+        controllerAs: 'sportCtrl'
+        
     });
-})
-
-.controller('sportsController', function($http){
-// this.sports = ["weightlifting", "cycling"];
-  $http.get('http://admin.zenzig.com:3000/api/sports').then((response) => {
-    this.sports = response.data;
-  });
 });
-
-
-
-
 
 
